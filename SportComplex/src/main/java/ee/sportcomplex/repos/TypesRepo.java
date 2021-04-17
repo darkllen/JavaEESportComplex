aa9cd245fadd30243729974b5c78f46fe2ffc433
darkllen <yankiniigor@gmail.com>
2021-04-18 02:25:18 +0300

    fix

--- SportComplex/src/main/java/ee/sportcomplex/repos/TypesRepo.java
+++ SportComplex/src/main/java/ee/sportcomplex/repos/TypesRepo.java
@@ -1,10 +1,12 @@
 package ee.sportcomplex.repos;
 
+import ee.sportcomplex.dto.Type;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
-public interface TypesRepo {
+public interface TypesRepo extends JpaRepository<Type, Integer> {
     @Query(value = "SELECT t from Type t")
     List<TypesShort> getAllShort();
     public interface TypesShort{
