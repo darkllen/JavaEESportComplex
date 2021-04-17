315c3703815028adaef100265e5fa5f7213f8f2f
darkllen <yankiniigor@gmail.com>
2021-04-18 00:54:16 +0300

    get_all_complexes

--- SportComplex/src/main/java/ee/sportcomplex/repos/ComplexRepo.java
+++ SportComplex/src/main/java/ee/sportcomplex/repos/ComplexRepo.java
@@ -3,13 +3,18 @@ package ee.sportcomplex.repos;
 import ee.sportcomplex.dto.Complex;
 import ee.sportcomplex.dto.Permissions;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface ComplexRepo extends JpaRepository<Complex, Integer> {
-    List<ComplexShort> findAllBy();
+    @Query(value = "SELECT c from Complex c")
+    List<ComplexShort> getAllShort();
     public interface ComplexShort{
         public int getId();
         public String getName();
     }
+
+    @Query(value = "SELECT c from Complex c")
+    List<Complex> getAll();
 }
--- SportComplex/src/main/java/ee/sportcomplex/services/ComplexService.java
+++ SportComplex/src/main/java/ee/sportcomplex/services/ComplexService.java
@@ -1,11 +1,14 @@
 package ee.sportcomplex.services;
 
+import ee.sportcomplex.dto.Complex;
 import ee.sportcomplex.repos.ComplexRepo;
 import ee.sportcomplex.repos.schedules.ScheduleGroupRepo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -13,6 +16,10 @@ public class ComplexService {
     private final ComplexRepo repo;
 
     public List<ComplexRepo.ComplexShort> getComplexesShort(){
-        return repo.findAllBy();
+        return repo.getAllShort();
+    }
+
+    public List<Complex> getComplexes(){
+        return repo.getAll();
     }
 }
