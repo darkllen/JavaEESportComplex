bc163d2e7bf69ba4beea54470867e406ac34326d
darkllen <yankiniigor@gmail.com>
2021-04-17 18:54:42 +0300

    init project

---
+++ SportComplex/src/main/java/ee/sportcomplex/SportComplexApplication.java
@@ -0,0 +1,13 @@
+package ee.sportcomplex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportComplexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SportComplexApplication.class, args);
+    }
+
+}
