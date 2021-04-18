bc13a0d1b5d94866beb25a3db85239d3af9d02f0
darkllen <yankiniigor@gmail.com>
2021-04-18 03:14:32 +0300

    add old_trainings trainings

--- SportComplex/src/main/java/ee/sportcomplex/services/ScheduleService.java
+++ SportComplex/src/main/java/ee/sportcomplex/services/ScheduleService.java
@@ -1,18 +1,31 @@
 package ee.sportcomplex.services;
 
 import ee.sportcomplex.dto.schedules.ScheduleGroup;
+import ee.sportcomplex.dto.schedules.ScheduleInd;
+import ee.sportcomplex.dto.users.Client;
 import ee.sportcomplex.repos.schedules.ScheduleGroupRepo;
+import ee.sportcomplex.repos.schedules.ScheduleIndRepo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ScheduleService {
-    private final ScheduleGroupRepo repo;
+    private final ScheduleGroupRepo groupRepo;
+    private final ScheduleIndRepo indRepo;
 
     public List<ScheduleGroup> getScheduleByDay(ScheduleGroup.DayOfWeek day){
-        return repo.findAllByDayOfWeek(day);
+        return groupRepo.findAllByDayOfWeek(day);
     }
+
+    public List<ScheduleInd> findAllByClientAndScheduleDateBefore(Client client, Date date){
+        System.out.println(indRepo.findAllByClientAndScheduleDateBefore(client,date));
+        return indRepo.findAllByClientAndScheduleDateBefore(client,date);
+    };
+    public List<ScheduleInd> findAllByClientAndScheduleDateAfter(Client client, Date date){
+        return indRepo.findAllByClientAndScheduleDateAfter(client,date);
+    };
 }
--- SportComplex/src/main/java/ee/sportcomplex/services/users/UserService.java
+++ SportComplex/src/main/java/ee/sportcomplex/services/users/UserService.java
@@ -1,22 +1,29 @@
 package ee.sportcomplex.services.users;
 
+import ee.sportcomplex.dto.users.Client;
 import ee.sportcomplex.dto.users.Coach;
 import ee.sportcomplex.repos.schedules.ScheduleGroupRepo;
+import ee.sportcomplex.repos.users.ClientRepo;
 import ee.sportcomplex.repos.users.CoachRepo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final CoachRepo coachRepo;
+    private final ClientRepo clientRepo;
 
     public List<CoachRepo.CoachShort> getCoachesShort(){
         return coachRepo.getAllShort();
     }
     public List<Coach> getCoaches(){
         return coachRepo.getAll();
+    }
+    public Optional<Client> getClientByLogin(String login){
+        return clientRepo.findClientsByLogin(login);
     }
 }
