bd54ceeb7fe2d8e5df5f5bb36b423f80ad436393
darkllen <yankiniigor@gmail.com>
2021-04-18 22:01:33 +0300

    get_shedule_by_day_of_week_by_trener

--- SportComplex/src/main/java/ee/sportcomplex/repos/schedules/ScheduleGroupRepo.java
+++ SportComplex/src/main/java/ee/sportcomplex/repos/schedules/ScheduleGroupRepo.java
@@ -2,10 +2,13 @@ package ee.sportcomplex.repos.schedules;
 
 import ee.sportcomplex.dto.schedules.ScheduleGroup;
 import ee.sportcomplex.dto.schedules.ScheduleInd;
+import ee.sportcomplex.dto.users.Coach;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface ScheduleGroupRepo extends JpaRepository<ScheduleGroup, Integer> {
     List<ScheduleGroup> findAllByDayOfWeek(ScheduleGroup.DayOfWeek day);
+
+    List<ScheduleGroup> findAllByDayOfWeekAndCoach(ScheduleGroup.DayOfWeek day, Coach coach);
 }
