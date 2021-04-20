4d75567244d488320f7bfea4784f92ace72bb4de
darkllen <yankiniigor@gmail.com>
2021-04-20 03:09:49 +0300

    edit_admin remove_admin

--- SportComplex/src/main/java/ee/sportcomplex/controllers/rest/RestUserController.java
+++ SportComplex/src/main/java/ee/sportcomplex/controllers/rest/RestUserController.java
@@ -39,7 +39,7 @@ public class RestUserController {
 
 
     @ResponseBody
-    @RequestMapping(value = {"/remove_coach"}, method = RequestMethod.POST)
+    @RequestMapping(value = {"/remove_coach", "/remove_admin"}, method = RequestMethod.POST)
     public ResponseEntity<String> remove_coach(@RequestBody Map<String, Integer> map){
         try{
             userService.setToClient(map.get("id"));
@@ -49,12 +49,14 @@ public class RestUserController {
         }
     }
 
+
     @ResponseBody
-    @RequestMapping(value = {"/edit_coach"}, method = RequestMethod.POST)
+    @RequestMapping(value = {"/edit_coach", "/edit_admin"}, method = RequestMethod.POST)
     public AuthUser edit_coach(@RequestBody @Valid AuthUser user){
         //TODO test
         return userService.editAuthUser(user);
     }
+
 
     @ResponseBody
     @RequestMapping(value = {"/change_user_info"}, method = RequestMethod.POST)
